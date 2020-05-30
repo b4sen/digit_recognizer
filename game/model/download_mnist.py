be7685163d377f331fffecd006d0a00a6ca67c8c
Kolos Niedermayer <kolos.niedermayer@gmail.com>
2020-05-30 19:34:55 +0200

    minor changes to relative path

--- game/model/download_mnist.py
+++ game/model/download_mnist.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import pickle
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-pickle.dump(x_train, open('data/x_train','wb'))
-pickle.dump(y_train, open('data/y_train','wb'))
-pickle.dump(x_test, open('data/x_test','wb'))
-pickle.dump(y_test, open('data/y_test','wb'))
+pickle.dump(x_train, open('../../data/x_train','wb'))
+pickle.dump(y_train, open('../../data/y_train','wb'))
+pickle.dump(x_test, open('../../data/x_test','wb'))
+pickle.dump(y_test, open('../../data/y_test','wb'))
